f6d1133b63ff31757e1d0bb6920db60238eaae0c
supershem24 <107944240+supershem24@users.noreply.github.com>
2024-04-11 20:40:38 +0000

    Trying to fix Supabase

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { createClient } = supabase
+const { createClient } = 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabaseUrl = 'https://cecpqnipsjocjrggycon.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlY3Bxbmlwc2pvY2pyZ2d5Y29uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3MDM5NjcsImV4cCI6MjAyNjI3OTk2N30.TvYs7xbE4Fn2Quus8utQBSFHYmtxEEjEX8raYjFtNlU'
